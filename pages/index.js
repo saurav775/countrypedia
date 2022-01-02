b64eda154d7d4f2ecd44eff25bd584566a868a9b
sauravZopsmart <saurav.choudhury@zopsmart.com>
2022-01-02 19:24:21 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>CountryPedia</title>
+      </Head>
+      <main>CountryPedia</main>
+    </div>
+  );
+}
